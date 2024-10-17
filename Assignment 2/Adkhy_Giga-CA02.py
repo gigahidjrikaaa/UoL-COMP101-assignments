7f4cd8559547887902f0a1c6d6c1d179d83f6a2d
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-10-17 13:11:31 +0100

    feat: adjustments

--- Assignment 2/Adkhy_Giga-CA02.py
+++ Assignment 2/Adkhy_Giga-CA02.py
@@ -1,11 +1,12 @@
 # Adkhy_Giga 201847335  October2024 CA-02.py
 # A
 
-min_angle = 0       # Minimum angle in degrees (North)
-max_angle = 90      # Maximum angle in degrees (East, inclusive)
+import math
+
 speed = 1.5         # Speed in m/s
 battery = 100       # Battery in percentage
 battery_usage = 2.7 # Battery usage in percentage per second
+status = {"RETURN_TRIP", "ONE_TRIP", "MID_RECHARGE_NEEDED"}       # Status of the travel
 
 # Inputs
 angle = float(input("Enter the angle in degrees: "))
@@ -13,24 +14,29 @@ travel_time = float(input("Enter the travel time in seconds: "))
 
 # Calculations
 distance = speed * travel_time
-horizontal_distance = distance * (angle / max_angle)
-vertical_distance = distance * ((max_angle - angle) / max_angle)
+# Horizontal distance travelled: distance multiplied by the sine of the angle
+horizontal_distance = distance * math.sin(math.radians(angle))
+# Vertical distance travelled: distance multiplied by the cosine of the angle
+vertical_distance = distance * math.cos(math.radians(angle))
 estimated_battery_usage = travel_time * battery_usage
 
 # Outputs
 print("\nThe angle is: ", angle)
 print("The travel time is: ", travel_time)
+print("The distance travelled is: ", distance)
+print("The horizontal distance travelled is: ", horizontal_distance)
+print("The vertical distance travelled is: ", vertical_distance)
+print("The estimated battery usage is: ", estimated_battery_usage)
+print("The battery is at: ", battery - estimated_battery_usage, "%.")
 
 # Check if robot can travel the distance
-if estimated_battery_usage > battery:
-    print("The robot has travelled", horizontal_distance, "m horizontally and", vertical_distance, "m vertically. Total distance travelled is", distance, "m.")
+if battery - estimated_battery_usage < 0:
     print("The battery is not enough to travel the distance.")
+    print("Solar recharge is needed.")
 else:
-    print("The robot has travelled", horizontal_distance, "m horizontally and", vertical_distance, "m vertically. Total distance travelled is", distance, "m.")
-    print("The battery is at", battery - estimated_battery_usage, "%.")
     if battery - estimated_battery_usage > estimated_battery_usage:
         print("The battery is enough to travel back to the starting point. No solar recharge is needed.")
     else:
         print("The battery is not enough to travel back to the starting point. Solar recharge is needed.")
-        
+   
 

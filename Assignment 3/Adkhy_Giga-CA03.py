c0e9d08096381ec76f72f84c19a8ec5fc196e901
Giga Hidjrika Aura Adkhy <gigahidjrikaaa@gmail.com>
2024-10-22 10:42:11 +0100

    fix: typo

--- Assignment 3/Adkhy_Giga-CA03.py
+++ Assignment 3/Adkhy_Giga-CA03.py
@@ -13,7 +13,7 @@ prompt = input("Prompt: ")
 print("===================================================")
 # valid_prompts = ['A', 'a', 'B', 'b', 'C', 'c', 'X', 'x']
 invalid_message = "Choice invalid. Exiting..."
-about = "Cat age calculator is used to find the equivalent of cat age in human years.\nThere are different calculations for each life stage of a cat. They are:\n1. Kitten: 0 - 7 months\n2. Junior: 8 - 24 months\n3. Prime: 3 - 6 years\n4.Mature: 7 - 10 years\n5. Senior: 11 - 14 years\n6. Geriatric: 15 - 18 years.\n"
+about = "Cat age calculator is used to find the equivalent of cat age in human years.\nThere are different calculations for each life stage of a cat. They are:\n1. Kitten: 0 - 7 months\n2. Junior: 8 - 24 months\n3. Prime: 3 - 6 years\n4. Mature: 7 - 10 years\n5. Senior: 11 - 14 years\n6. Geriatric: 15 - 18 years.\n"
 references_list = "Further reading can be seen here:\n1. "
 
 if prompt == 'A' or prompt == 'a':

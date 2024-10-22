54e5a3295166521bebef2c9fdbce32b65bd04def
Giga Hidjrika Aura Adkhy <gigahidjrikaaa@gmail.com>
2024-10-22 10:15:15 +0100

    feat: desc update

--- Assignment 3/Adkhy_Giga-CA03.py
+++ Assignment 3/Adkhy_Giga-CA03.py
@@ -1,4 +1,5 @@
 # Adkhy_Giga 201847335  October2024 CA-03.py
-# Finding out if a rover can return to its starting point after travelling a certain time and angle, also provide rover status.
+# Calculating the age of a cat in human years.
+
 
 

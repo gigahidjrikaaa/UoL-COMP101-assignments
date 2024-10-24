13420fc62628db89e83d2ad653f962d3a324e3f8
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-10-24 12:56:57 +0100

    feat: added comments

--- Assignment 3/Adkhy_Giga-CA03.py
+++ Assignment 3/Adkhy_Giga-CA03.py
@@ -17,6 +17,7 @@ invalid_message = "Choice invalid. Exiting..."
 about = "Cat age calculator is used to find the equivalent of cat age in human years.\nThere are different calculations for each life stage of a cat. They are:\n1. Kitten: 0 - 7 months\n2. Junior: 8 - 24 months\n3. Prime: 3 - 6 years\n4. Mature: 7 - 10 years\n5. Senior: 11 - 14 years\n6. Geriatric: 15 - 18 years.\n"
 references_list = "Further reading can be seen here:\n1. "
 
+# If A, else if B, else if C, else if X is chosen
 if prompt == 'A' or prompt == 'a':
     age = int(input("Age in months (8, 12, 18, or 24): "))
     human_years = age + 3
@@ -36,6 +37,7 @@ elif prompt == 'C' or prompt == 'c':
     print(references_list)
 elif prompt == 'X' or prompt == 'x':
     print("Exiting... Have a good day.")
+# If invalid input
 else:
     print(invalid_message)
 

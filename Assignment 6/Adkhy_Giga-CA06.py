c59534864c90e32f71ae86c790b535e1bad6ef55
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-11-28 14:23:00 +0000

    feat: comments and formatting

--- Assignment 6/Adkhy_Giga-CA06.py
+++ Assignment 6/Adkhy_Giga-CA06.py
@@ -26,22 +26,22 @@ def getDirectorsName(alien_names):
     '''
     Takes in a list of alien names and returns the director's name. Director name is hidden in the alien names.
     '''
-    # Initialize variable to store director's name
+    # Initialize empty string to store director's name
     director_name = ""
-    # Loop through the alien names and combine the first 2 letters of each alien name to get the director's name (stevenspielberg)
+    # Loop through the alien names and combine the first 3 letters of each alien name to get the director's name (stevenspielberg)
     for i in range(len(alien_names)):
         director_name += alien_names[i][:3]
 
     return director_name
 
-def printLine(width=70):
+def printLine(width=80):
     '''
     Prints a line of width times equal signs.
     '''
     print("=" * width)
     return
 
-def printCentered(text, width=70):
+def printCentered(text, width=80):
     '''
     Takes in a string and prints it out centered on the screen (relative to width).
     '''
@@ -53,16 +53,16 @@ def printCredits(actor_first_names, actor_last_names, alien_names, director_name
     Takes in a list of alien names and prints them out in a formatted way.
     '''
     printLine()
-    printCentered("Directed by")
     printCentered(director_name)
+    printCentered("Presents")
     printLine()
     printCentered("ALIEN")
     printLine()
     printCentered("Starring")
-    print("The Cast:\t\t played the part of: \t\tAlien Name:")
+    print("The Cast:\t\t       played the part of: \t\tAlien Name:")
     # Loop through the alien names and print them out in a formatted way
     for i in range(len(alien_names)):
-        print(actor_first_names[i], actor_last_names[i], "\t\t\t\t\t\t" + alien_names[i])
+        print(actor_first_names[i], actor_last_names[i], "     \t\t\t\t\t\t" + alien_names[i])
 
     return
 

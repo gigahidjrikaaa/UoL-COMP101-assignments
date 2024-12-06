f1048fc123b38a10ff9c1b4ef81fee69d6ca6f8e
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-12-06 16:29:00 +0000

    feat: initial input

--- Assignment 7/Adkhy_Giga-CA07.py
+++ Assignment 7/Adkhy_Giga-CA07.py
@@ -1,8 +1,16 @@
 # Adkhy_Giga Nov2024 CA-07.py
 # 
 
+def breakEven(production_cost, revenue):
+    '''
+    Takes in the production cost and selling price for band A and calculates the break-even point.
+    '''
+    break_even_point = production_cost / revenue
+
+
 def main():
-    print("CA-07: Adkhy Giga")
+    production_cost = int(input("Enter the production cost: "))
+    selling_price_A = int(input("Enter the selling price for band A: "))
 
 if __name__ == "__main__":
     main()

9792d53922f104d95bc5460b086af56fdd072fa5
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-12-11 22:21:47 +0000

    feat: BEP and inputs

--- Assignment 7/Adkhy_Giga-CA07.py
+++ Assignment 7/Adkhy_Giga-CA07.py
@@ -1,16 +1,29 @@
-# Adkhy_Giga Nov2024 CA-07.py
-# 
+# Adkhy_Giga Dec2024 CA-07.py
+# Breaking-Even Point for Theatre Seating Dashboard
 
-def breakEven(production_cost, revenue):
+def getBreakEvenPoint(production_cost, revenue):
     '''
     Takes in the production cost and selling price for band A and calculates the break-even point.
     '''
     break_even_point = production_cost / revenue
+    rounded_break_even_point = int(-(-break_even_point // 1))
+    return rounded_break_even_point
 
+def getInputs():
+    '''
+    Takes in the production cost and selling price for band A.
+    '''
+    production_cost = int(input("Enter the production cost: "))
+    price_seat_A = int(input("Enter the selling price for seat at band A: "))
+    price_seat_B = 2 * price_seat_A
+    return production_cost, price_seat_A, price_seat_B
 
 def main():
-    production_cost = int(input("Enter the production cost: "))
-    selling_price_A = int(input("Enter the selling price for band A: "))
+    '''
+    Main function to call the other functions.
+    '''
+    production_cost, price_seat_A, price_seat_B = getInputs()
+
 
 if __name__ == "__main__":
     main()
